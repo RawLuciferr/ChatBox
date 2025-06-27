4cc445be64865856af15e47126b8cf7a1cf8e62c
RawLuciferr <121742156+RawLuciferr@users.noreply.github.com>
2025-06-27 14:14:13 +0530

    Done

---
+++ my-app/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+import { devIndicatorServerState } from "next/dist/server/dev/dev-indicator-server-state";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+devIndicatorServerState:false;
